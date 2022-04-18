20a3728aa93a24a9e59f988b09c487f9e4130830
Kiran <kiranhulawale123@gmail.com>
2022-04-18 11:41:30 +0530

    Added UC-1 to add the hotels

--- src/main/java/com/bridgelabz/Hotel.java
+++ src/main/java/com/bridgelabz/Hotel.java
@@ -1,5 +1,60 @@
 package com.bridgelabz;
 
 public class Hotel {
+    private String hotelName;
+    private String WeekDayRegularRate;
+    private String WeekDayRewardRate;
+    private String WeekEndRegularRate;
+    private String WeekEndRewardRate;
 
+    public String getHotelName() {
+        return hotelName;
+    }
+
+    public void setHotelName(String hotelName) {
+        this.hotelName = hotelName;
+    }
+
+    public String getWeekDayRegularRate() {
+        return WeekDayRegularRate;
+    }
+
+    public void setWeekDayRegularRate(String wDayRegularRate) {
+        this.WeekDayRegularRate = wDayRegularRate;
+    }
+
+    public String getWeekDayRewardRate() {
+        return WeekDayRewardRate;
+    }
+
+    public void setWeekDayRewardRate(String wDayRewardRate) {
+        this.WeekDayRewardRate = wDayRewardRate;
+    }
+
+    public String getWeekEndRegularRate() {
+        return WeekEndRegularRate;
+    }
+
+    public void setWeekEndRegularRate(String wEndRegularRate) {
+        this.WeekEndRegularRate = wEndRegularRate;
+    }
+
+    public String getWeekEndRewardRate() {
+        return WeekEndRewardRate;
+    }
+
+    public void setWeekEndRewardRate(String wEndRewardRate) {
+        this.WeekEndRewardRate = wEndRewardRate;
+    }
+
+    @Override
+    public String toString() {
+        return "Hotel{" +
+                "hotelName='" + hotelName + '\'' +
+                ", WeekDayRegularRate='" + WeekDayRegularRate + '\'' +
+                ", WeekDayRewardRate='" + WeekDayRewardRate + '\'' +
+                ", WeekEndRegularRate='" + WeekEndRegularRate + '\'' +
+                ", WeekEndRewardRate='" + WeekEndRewardRate + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/bridgelabz/HotelReservationSystem.java
+++ src/main/java/com/bridgelabz/HotelReservationSystem.java
@@ -3,8 +3,51 @@ import java.util.*;
 
 public class HotelReservationSystem {
 
+    private static final Scanner scanner = new Scanner(System.in);
+    private static List<Hotel> hotelsList = new ArrayList<>();
+
     public static void main(String[] args) {
         System.out.println("Welcome to hotel reservation system");
-
+        HotelReservationSystem hotelReservationMain = new HotelReservationSystem();
+        boolean exit = false;
+        do {
+            System.out.println("\tWelcome To Hotel Reservation\n");
+            System.out.println("\tEnter A to Add Hotel\n");
+            System.out.print("\tPlease Select One Option :\n ");
+            char userInput = scanner.nextLine().toUpperCase().charAt(0);
+            switch (userInput) {
+                case 'A':
+                    //add
+                    hotelReservationMain.addHotel();
+                    break;
+                case 'Q':
+                    //quit
+                    exit = true;
+                    break;
+                default:
+                    System.out.println("Please select correct option");
+            }
+        } while (!exit);
     }
+    public boolean addHotel(){
+        Hotel hotel = new Hotel();
+        try {
+            System.out.print("Enter Hotel Name : ");
+            hotel.setHotelName(scanner.nextLine());
+            System.out.print("Enter Weekdays Regular Rate : ");
+            hotel.setWeekDayRegularRate(scanner.nextLine());
+            System.out.print("Enter Weekdays Reward Rate : ");
+            hotel.setWeekDayRewardRate(scanner.nextLine());
+            System.out.print("Enter Weekend Regular Rate : ");
+            hotel.setWeekEndRegularRate(scanner.nextLine());
+            System.out.print("Enter Weekend Reward Rate : ");
+            hotel.setWeekEndRewardRate(scanner.nextLine());
+            hotelsList.add(hotel);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
+
 }
+
