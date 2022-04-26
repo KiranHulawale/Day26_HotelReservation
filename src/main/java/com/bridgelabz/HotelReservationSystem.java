cbc6c8f04aaeace20965fe3130647aa66f4c8621
Kiran <kiranhulawale123@gmail.com>
2022-04-26 21:27:16 +0530

    Added UC-3 to add Weekend and Weekdays rates

--- src/main/java/com/bridgelabz/HotelReservationSystem.java
+++ src/main/java/com/bridgelabz/HotelReservationSystem.java
@@ -1,6 +1,4 @@
 package com.bridgelabz;
-import java.util.*;
-
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -8,25 +6,31 @@ import java.util.Objects;
 
 public class HotelReservationSystem {
     List<Hotel> hotelDetails = new ArrayList<>();
+
     public void addHotelDetails(String hotelName, int weekdayRateForRegular, int weekendRateForRegular, int weekdayRateForRewarder, int weekendRateForRewarder) {
-        Hotel hotel= new Hotel(hotelName, weekdayRateForRegular, weekendRateForRegular, weekdayRateForRewarder, weekendRateForRewarder);
+        Hotel hotel = new Hotel(hotelName, weekdayRateForRegular, weekendRateForRegular, weekdayRateForRewarder, weekendRateForRewarder);
         hotelDetails.add(hotel);
     }
-    public void getHotelDetails(){
+
+    public void getHotelDetails() {
         System.out.println(hotelDetails);
     }
+
     public boolean checkHotelDetails(Hotel hotel) {
         return hotelDetails.add(hotel);
     }
+
     public void getMinPriceHotel() {
         Hotel cheapestHotel = hotelDetails.stream().min(Comparator.comparing(Hotel::getWeekdayRateForRegularCustomer)).orElseThrow();
         System.out.println(cheapestHotel);
     }
 
-    public void getCheapestHotel(){
-        int LakewoodRate = 110; int BridgewoodRate = 160; int Ridgewood = 220;
-        int cheapestHotel = (LakewoodRate>BridgewoodRate) ? (Math.max(LakewoodRate, Ridgewood)) : (Ridgewood) ;
-        System.out.println("The cheapest Hotel Rate is " +cheapestHotel);
+    public void getCheapestHotel() {
+        int LakewoodRate = 110;
+        int BridgewoodRate = 160;
+        int Ridgewood = 220;
+        int cheapestHotel = (LakewoodRate > BridgewoodRate) ? (Math.max(LakewoodRate, Ridgewood)) : (Ridgewood);
+        System.out.println("The cheapest Hotel Rate is " + cheapestHotel);
     }
 
     public void viewHotels() {
@@ -34,4 +38,3 @@ public class HotelReservationSystem {
         getCheapestHotel();
     }
 }
-
