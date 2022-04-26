ee19c110a849468a90557774d9e43057af5bbb95
Kiran <kiranhulawale123@gmail.com>
2022-04-26 21:16:12 +0530

    Added UC-1 to add the hotels

--- src/main/java/com/bridgelabz/HotelReservationSystem.java
+++ src/main/java/com/bridgelabz/HotelReservationSystem.java
@@ -1,53 +1,20 @@
 package com.bridgelabz;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
 
 public class HotelReservationSystem {
-
-    private static final Scanner scanner = new Scanner(System.in);
-    private static List<Hotel> hotelsList = new ArrayList<>();
-
-    public static void main(String[] args) {
-        System.out.println("Welcome to hotel reservation system");
-        HotelReservationSystem hotelReservationMain = new HotelReservationSystem();
-        boolean exit = false;
-        do {
-            System.out.println("\tWelcome To Hotel Reservation\n");
-            System.out.println("\tEnter A to Add Hotel\n");
-            System.out.print("\tPlease Select One Option :\n ");
-            char userInput = scanner.nextLine().toUpperCase().charAt(0);
-            switch (userInput) {
-                case 'A':
-                    //add
-                    hotelReservationMain.addHotel();
-                    break;
-                case 'Q':
-                    //quit
-                    exit = true;
-                    break;
-                default:
-                    System.out.println("Please select correct option");
-            }
-        } while (!exit);
+    List<Hotel> hotelDetails = new ArrayList<>();
+    public void addHotelDetails(String hotelName, int weekdayRateForRegular, int weekendRateForRegular, int weekdayRateForRewarder, int weekendRateForRewarder) {
+        Hotel hotel= new Hotel(hotelName, weekdayRateForRegular, weekendRateForRegular, weekdayRateForRewarder, weekendRateForRewarder);
+        hotelDetails.add(hotel);
     }
-    public boolean addHotel(){
-        Hotel hotel = new Hotel();
-        try {
-            System.out.print("Enter Hotel Name : ");
-            hotel.setHotelName(scanner.nextLine());
-            System.out.print("Enter Weekdays Regular Rate : ");
-            hotel.setWeekDayRegularRate(scanner.nextLine());
-            System.out.print("Enter Weekdays Reward Rate : ");
-            hotel.setWeekDayRewardRate(scanner.nextLine());
-            System.out.print("Enter Weekend Regular Rate : ");
-            hotel.setWeekEndRegularRate(scanner.nextLine());
-            System.out.print("Enter Weekend Reward Rate : ");
-            hotel.setWeekEndRewardRate(scanner.nextLine());
-            hotelsList.add(hotel);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return true;
+    public void getHotelDetails(){
+        System.out.println(hotelDetails);
     }
-
+    public boolean checkHotelDetails(Hotel hotel) {
+        return hotelDetails.add(hotel);
+    }
 }
 
---
+++ src/test/java/com/bridgelabz/HotelReservationTest.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+import org.junit.Before;
+
+public class HotelReservationTest {
+    public HotelReservationSystem hotelReservationMain;
+
+    @Before
+    public void setup(){
+        hotelReservationMain = new HotelReservationSystem();
+    }
+}
+
